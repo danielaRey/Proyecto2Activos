d142c97e806d9117fa32857e1a3a8f5da2fee3f8
oscarrp98 <44251622+oscarrp98@users.noreply.github.com>
2018-10-19 22:23:27 -0600

    paquetes

---
+++ Proyecto2/src/proyecto2/presentation/funcionarios/edicion/FuncionarioController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto2.presentation.funcionarios.edicion;
+
+/**
+ *
+ * @author oscar
+ */
+public class FuncionarioController {
+    
+}
